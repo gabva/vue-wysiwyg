e0c45fd37f88cb9d1fe0110179288d6fdb95ddff
Gabriel Deschildre <gabriel.deschildre@egdweb.fr>
2020-05-07 14:20:51 +0200

    Create french.js

---
+++ src/editor/locales/french.js
@@ -0,0 +1,20 @@
+module.exports = {
+    locale_name: 'French',
+    locale_shorthand: ['fr', 'french'],
+    // Translations:
+    justifyCenter: 'Centrer',
+    justifyLeft: 'Justifier Gauche',
+    justifyRight: 'Justifier Droite',
+    bold: 'Gras',
+    code: 'Code',
+    headings: 'Titres (h1-h6)',
+    link: 'Hyperlien',
+    image: 'Inserer une image',
+    italic: 'Italique',
+    orderedList: 'Liste numérotée (1, 2, 3, ...)',
+    unorderedList: 'Liste à bulles',
+    removeFormat: 'Retirer le formattagedu texte.\nSupprime titres, gras, italiques etc.',
+    separator: null,
+    table: 'Inserer un tableau',
+    underline: 'Souligner'
+}
